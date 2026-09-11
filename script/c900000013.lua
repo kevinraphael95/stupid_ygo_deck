ce41905d256d53140e5e23c42087d0f6dcf70238
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-09-12 00:19:21 +0200

    Update c900000013.lua

--- script/c900000013.lua
+++ script/c900000013.lua
@@ -1,28 +1,28 @@
+--JAIME PAS LIRE
 local s,id=GetID()
-function s.initial_effect(c)
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_DISABLE)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.activate)
-    c:RegisterEffect(e1)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+    local g = Duel.GetMatchingGroup(Card.IsFaceup, tp, LOCATION_MZONE, LOCATION_MZONE, nil)
     for tc in aux.Next(g) do
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_DISABLE)
-        e1:SetReset(RESET_PHASE+PHASE_END)
-        tc:RegisterEffect(e1)
-        local e2=Effect.CreateEffect(e:GetHandler())
-        e2:SetType(EFFECT_TYPE_SINGLE)
-        e2:SetCode(EFFECT_DISABLE_EFFECT)
-        e2:SetReset(RESET_PHASE+PHASE_END)
-        tc:RegisterEffect(e2)
+        -- Vérifie si le monstre a été invoqué depuis l'Extra Deck
+        local isExtraDeckMonster = tc:IsSummonType(SUMMON_TYPE_FUSION)
+            or tc:IsSummonType(SUMMON_TYPE_SYNCHRO)
+            or tc:IsSummonType(SUMMON_TYPE_XYZ)
+            or tc:IsSummonType(SUMMON_TYPE_LINK)
+            or tc:IsSummonType(SUMMON_TYPE_PENDULUM)
+
+        -- Si ce n'est PAS un monstre invoqué depuis l'Extra Deck, on annule ses effets
+        if not isExtraDeckMonster then
+            local e1 = Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_DISABLE)
+            e1:SetReset(RESET_PHASE + PHASE_END)
+            tc:RegisterEffect(e1)
+
+            local e2 = Effect.CreateEffect(e:GetHandler())
+            e2:SetType(EFFECT_TYPE_SINGLE)
+            e2:SetCode(EFFECT_DISABLE_EFFECT)
+            e2:SetReset(RESET_PHASE + PHASE_END)
+            tc:RegisterEffect(e2)
+        end
     end
 end
