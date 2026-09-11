c44eb8e615a0523bc2caa403f4394d813caf5814
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-09-11 22:59:36 +0200

    Update c900000008.lua

--- script/c900000008.lua
+++ script/c900000008.lua
@@ -4,10 +4,17 @@ function s.initial_effect(c)
     e1:SetCategory(CATEGORY_DAMAGE)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetTarget(s.target)
     e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,2000)
+end
+
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    -- Seul l'adversaire (1-tp) a un travail et perd 2000 LP, le joueur (tp) ne perd rien
     Duel.Damage(1-tp,2000,REASON_EFFECT)
-    Duel.Damage(tp,2000,REASON_EFFECT)
 end
