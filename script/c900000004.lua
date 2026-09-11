f5e1566e376e50e6c6d97ca56921be2a3b84c3a9
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-09-11 23:10:32 +0200

    Update c900000004.lua

--- script/c900000004.lua
+++ script/c900000004.lua
@@ -1,93 +1,138 @@
+--Kévin
 local s,id=GetID()
 function s.initial_effect(c)
-    c:EnableReviveLimit()
-    -- Invocation Spéciale quand un monstre est invoqué
-    local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
-    e1:SetRange(LOCATION_HAND)
-    e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
-    c:RegisterEffect(e1)
-    local e2=e1:Clone()
-    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-    c:RegisterEffect(e2)
+	--Non Invocable Normalement
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_CANNOT_SUMMON)
+	e0:SetRange(LOCATION_HAND)
+	c:RegisterEffect(e0)
 
-    -- Protection en position de défense
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetCondition(s.defcon)
-    e3:SetValue(aux.tgoval)
-    c:RegisterEffect(e3)
-    local e4=e3:Clone()
-    e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-    e4:SetValue(aux.indoval)
-    c:RegisterEffect(e4)
+	--Invocation Spéciale depuis la main si un monstre est Invoqué (Normal ou Spécial)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	local e1c=e1:Clone()
+	e1c:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e1c)
 
-    -- Effet Rapide : Mélanger la main
-    local e5=Effect.CreateEffect(c)
-    e5:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
-    e5:SetType(EFFECT_TYPE_QUICK_O)
-    e5:SetCode(EVENT_FREE_CHAIN)
-    e5:SetRange(LOCATION_MZONE)
-    e5:SetCountLimit(1)
-    e5:SetTarget(s.handtg)
-    e5:SetOperation(s.handop)
-    c:RegisterEffect(e5)
+	--Inciblable par les effets adverses tant qu'en Position de Défense
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,1)
+	e2:SetCondition(s.defcon)
+	c:RegisterEffect(e2)
 
-    -- Retour en main si ciblé en position d'attaque
-    local e6=Effect.CreateEffect(c)
-    e6:SetCategory(CATEGORY_TOHAND)
-    e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e6:SetCode(EVENT_BECOME_TARGET)
-    e6:SetCondition(s.atkcon)
-    e6:SetTarget(s.thtg)
-    e6:SetOperation(s.thop)
-    c:RegisterEffect(e6)
+	--Indestructible par effet adverse tant qu'en Position de Défense
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,1)
+	e3:SetCondition(s.defcon)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+
+	--Effet Rapide, une fois par tour : mélanger les 2 mains dans le Deck puis piocher
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetTarget(s.handtg)
+	e4:SetOperation(s.handop)
+	c:RegisterEffect(e4)
+
+	--Retour en main si ciblée par une ATTAQUE en Position d'Attaque
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,2))
+	e5:SetCategory(CATEGORY_TOHAND)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_BE_BATTLE_TARGET)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCondition(s.atkcon)
+	e5:SetTarget(s.thtg)
+	e5:SetOperation(s.thop)
+	c:RegisterEffect(e5)
+
+	--Retour en main si ciblée par un EFFET ADVERSE en Position d'Attaque
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,2))
+	e6:SetCategory(CATEGORY_TOHAND)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e6:SetCode(EVENT_BECOME_TARGET)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCondition(s.atktgcon)
+	e6:SetTarget(s.thtg)
+	e6:SetOperation(s.thop)
+	c:RegisterEffect(e6)
 end
+
+--Invocation Spéciale depuis la main
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsRelateToEffect(e) then
-        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-    end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsLocation(LOCATION_HAND)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
+
+--Condition Position de Défense
 function s.defcon(e)
-    return e:GetHandler():IsPosition(POS_DEFENSE)
+	return e:GetHandler():IsPosition(POS_DEFENSE)
 end
+
+--Effet Rapide : mélange des mains + pioche
 function s.handtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0 
-        and Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>0 end
-    Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,0,PLAYER_ALL,LOCATION_HAND)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,0,PLAYER_ALL,LOCATION_HAND)
 end
 function s.handop(e,tp,eg,ep,ev,re,r,rp)
-    local h1=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
-    local h2=Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)
-    local g=Duel.GetFieldGroup(tp,LOCATION_HAND,LOCATION_HAND)
-    if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 then
-        Duel.ShuffleDeck(tp)
-        Duel.ShuffleDeck(1-tp)
-        Duel.Draw(tp,h1,REASON_EFFECT)
-        Duel.Draw(1-tp,h2,REASON_EFFECT)
-    end
+	local h1=Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)
+	local h2=Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)
+	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,LOCATION_HAND)
+	if g:GetCount()>0 then
+		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+		Duel.ShuffleDeck(tp)
+		Duel.ShuffleDeck(1-tp)
+	end
+	if h1>0 then Duel.Draw(tp,h1,REASON_EFFECT) end
+	if h2>0 then Duel.Draw(1-tp,h2,REASON_EFFECT) end
 end
-function s.atkcon(e)
-    return e:GetHandler():IsPosition(POS_ATTACK)
+
+--Conditions du retour en main
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPosition(POS_ATTACK)
+end
+function s.atktgcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPosition(POS_ATTACK) and rp==1-tp
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    if e:GetHandler():IsRelateToEffect(e) then
-        Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
-    end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() and c:IsPosition(POS_ATTACK) then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,c)
+	end
 end
