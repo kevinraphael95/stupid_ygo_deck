11ccba835b1bea6d18f515afe1d29c8ef035e790
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-09-12 14:01:01 +0200

    Refactor effects for graveyard interaction
    Refactored effects to disable graveyard activations and redirect cards to banish instead of graveyard.

--- script/c900000010.lua
+++ script/c900000010.lua
@@ -1,31 +1,31 @@
-local s,id=GetID()
-function s.initial_effect(c)
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetOperation(s.activate)
-    c:RegisterEffect(e1)
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    -- Annule les effets déclenchés au Cimetière
-    local e1=Effect.CreateEffect(e:GetHandler())
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_CANNOT_ACTIVATE)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetTargetRange(1,1)
-    e1:SetValue(s.aclimit)
-    e1:SetReset(RESET_PHASE+PHASE_END)
-    Duel.RegisterEffect(e1,tp)
+--Non t'as pas de cimetière
+-- MAGIE CONTINUE
+-- "J'oublie ce qu'il y a dans ton cimetière, donc t'as pas le droit de jouer avec."
+-- Effet 1 : annule l'activation de tout effet de carte présent au cimetière
+-- Effet 2 : toute carte qui devrait aller au cimetière est bannie à la place
 
-    -- Banni toutes les cartes envoyées au Cimetière
-    local e2=Effect.CreateEffect(e:GetHandler())
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_TO_GRAVE_REDIRECT)
-    e2:SetTargetRange(LOCATION_ALL,LOCATION_ALL)
-    e2:SetValue(LOCATION_REMOVED)
-    e2:SetReset(RESET_PHASE+PHASE_END)
-    Duel.RegisterEffect(e2,tp)
-end
-function s.aclimit(e,re,tp)
-    return re:GetActivateLocation()==LOCATION_GRAVE
+local s,id = GetID()
+
+function s.initial_effect(c)
+
+	-------------------------------------------------
+	-- EFFET 1 : annule l'activation des effets depuis le cimetière
+	-------------------------------------------------
+	local e1 = Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_DISABLE_EFFECT)     -- annule les effets des cartes ciblées
+	e1:SetRange(LOCATION_SZONE)           -- cette magie continue doit être face recto sur le terrain
+	e1:SetTargetRange(LOCATION_GRAVE,LOCATION_GRAVE) -- cible : cartes dans N'IMPORTE QUEL cimetière (les deux joueurs)
+	c:RegisterEffect(e1)
+
+	-------------------------------------------------
+	-- EFFET 2 : toute carte qui irait au cimetière est bannie à la place
+	-------------------------------------------------
+	local e2 = Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_TO_GRAVE_REDIRECT)  -- redirige "envoi au cimetière" vers ailleurs
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetValue(LOCATION_REMOVED)         -- destination de remplacement : le banni
+	e2:SetTargetRange(LOCATION_ONFIELD+LOCATION_HAND+LOCATION_DECK+LOCATION_EXTRA,LOCATION_ONFIELD+LOCATION_HAND+LOCATION_DECK+LOCATION_EXTRA) -- affecte les deux joueurs, toutes zones d'origine possibles
+	c:RegisterEffect(e2)
 end
