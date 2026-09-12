0bab4636802654ffd56fcff425b253b1a4e68a06
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-09-12 13:45:02 +0200

    Update c900000002.lua

--- script/c900000002.lua
+++ script/c900000002.lua
@@ -1,18 +1,23 @@
--- Je la connais pas cette carte
+--Je la connais pas cette carte
 local s,id=GetID()
 function s.initial_effect(c)
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetOperation(s.activate)
-    c:RegisterEffect(e1)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-    local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-    if #g>0 then
-        Duel.HintSelection(g)
-        -- Message informatif en jeu pour demander à l'adversaire de lire sa carte
-        Debug.Message("L'adversaire doit expliquer la carte sélectionnée sans mentir !")
-    end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_MESSAGE,tp,aux.Stringid(id,0)) -- message pour TOI : choisis la carte
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		local op=1-tp
+		Duel.Hint(HINT_CARD,op,tc:GetCode())          -- montre la carte à l'adversaire
+		Duel.Hint(HINT_MESSAGE,op,aux.Stringid(id,1)) -- message pour L'ADVERSAIRE : explique
+	end
 end
