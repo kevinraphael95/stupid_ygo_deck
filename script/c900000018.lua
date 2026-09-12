fd87675c545dd1e77a36964443627d18d9b99b46
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-09-12 14:08:21 +0200

    Refactor effects for Flamed Winged Man

--- script/c900000018.lua
+++ script/c900000018.lua
@@ -1,32 +1,46 @@
-local s,id=GetID()
+--Flamed Winged Man
+-- MONSTRE FUSION
+-- N'est pas affecté par les effets de monstres FEU et VENT.
+-- Peut attaquer à nouveau après avoir attaqué un monstre FEU ou VENT.
+
+local s,id = GetID()
+
 function s.initial_effect(c)
-    c:EnableReviveLimit()
 
-    -- Immunité contre FEU et VENT
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCode(EFFECT_IMMUNE_EFFECT)
-    e1:SetValue(s.efilter)
-    c:RegisterEffect(e1)
+	-------------------------------------------------
+	-- EFFET 1 : immunité aux effets des monstres FEU et VENT
+	-------------------------------------------------
+	local e1 = Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetValue(s.immunefilter)  -- filtre : quelles cartes sont bloquées
+	c:RegisterEffect(e1)
 
-    -- Attaquer à nouveau après avoir attaqué FEU ou VENT
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_BATTLED)
-    e2:SetCondition(s.atkcon)
-    e2:SetOperation(s.atkop)
-    c:RegisterEffect(e2)
+	-------------------------------------------------
+	-- EFFET 2 : attaque supplémentaire après avoir attaqué un monstre FEU ou VENT
+	-------------------------------------------------
+	local e2 = Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EXTRA_ATTACK)
+	e2:SetValue(1)                -- 1 attaque supplémentaire
+	e2:SetCondition(s.atkcon)     -- seulement si la cible était FEU ou VENT
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e2)
 end
-function s.efilter(e,te)
-    return te:IsActiveType(TYPE_MONSTER) and (te:GetHandler():IsAttribute(ATTRIBUTE_FIRE) or te:GetHandler():IsAttribute(ATTRIBUTE_WIND))
+
+--------------------------------------------------
+-- FILTRE EFFET 1 : bloque les effets provenant de monstres FEU ou VENT
+--------------------------------------------------
+function s.immunefilter(e)
+	local ec = e:GetOwner()
+	return ec and ec:IsMonster() and (ec:IsAttribute(ATTRIBUTE_FIRE) or ec:IsAttribute(ATTRIBUTE_WIND))
 end
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local bc=c:GetBattleTarget()
-    return bc and (bc:IsAttribute(ATTRIBUTE_FIRE) or bc:IsAttribute(ATTRIBUTE_WIND)) and c:CanChainAttack()
-end
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.ChainAttack()
+
+--------------------------------------------------
+-- CONDITION EFFET 2 : vérifie que le monstre attaqué était FEU ou VENT
+--------------------------------------------------
+function s.atkcon(e)
+	local c = e:GetHandler()
+	local bc = c:GetBattleTarget() -- le monstre qui vient d'être attaqué
+	return bc and (bc:IsAttribute(ATTRIBUTE_FIRE) or bc:IsAttribute(ATTRIBUTE_WIND))
 end
