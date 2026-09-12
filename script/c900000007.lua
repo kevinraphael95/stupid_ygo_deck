55897ec595490b581f718d2f7782541502e523e0
Kévin <54324251+kevinraphael95@users.noreply.github.com>
2026-09-12 13:57:16 +0200

    Update c900000007.lua

--- script/c900000007.lua
+++ script/c900000007.lua
@@ -1,21 +1,30 @@
-local s,id=GetID()
+--Perdu ? Moi ? Sérieusement
+-- MAGIE RAPIDE
+-- Si le propriétaire devrait perdre la partie (LP à 0 ou deck-out) :
+-- il ne perd pas, la cause de la défaite est bannie, l'adversaire termine son tour immédiatement
+
+local s,id = GetID()
+
 function s.initial_effect(c)
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetCode(EVENT_CHAINING)
-    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-    e1:SetCondition(s.condition)
-    e1:SetOperation(s.activate)
-    c:RegisterEffect(e1)
+	local e1 = Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_AVOID_LOSE)  -- flag reconnu par le moteur pour éviter LP=0 / deck-out
+	e1:SetTargetRange(1,0)         -- 1 = protège TON camp uniquement
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1)
+
+	local e2 = Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetOperation(s.activate)
+	c:RegisterEffect(e2)
 end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-    -- Déclenchable sur un effet mortel
-    return rp~=tp
-end
+
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-    local rc=re:GetHandler()
-    if rc then
-        Duel.Remove(rc,POS_FACEUP,REASON_EFFECT)
-    end
-    Duel.SkipPhase(1-tp,Duel.GetCurrentPhase(),RESET_PHASE+PHASE_END,1)
+	local c=e:GetHandler()
+	local op=1-tp
+	-- bannit cette carte elle-même comme trace de "la cause neutralisée"
+	Duel.Remove(c,POS_FACEUP,REASON_EFFECT)
+	-- termine le tour de l'adversaire
+	Duel.ChangePhase(PHASE_END)
 end
